06232156c8c21aeee0626a3a6bf4d0499516c7e8
Alexander Castillo <alex.castillo@door3.com>
2015-02-10 13:20:40 -0500

    preparing for v1.0.7 release

--- angular-css.js
+++ angular-css.js
@@ -1,6 +1,6 @@
 /**
  * AngularCSS - CSS on-demand for AngularJS
- * @version v1.0.6
+ * @version v1.0.7
  * @author DOOR3, Alex Castillo
  * @link http://door3.github.io/angular-css
  * @license MIT License, http://www.opensource.org/licenses/MIT
